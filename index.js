7d2cc33aaa8d4a4f0f816e195c193ae4a5396ec3
Kashif Ali <jaxksahif34@gmail.com>
2022-10-23 18:26:13 +0500

    writting server-side code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fastify = require('fastify');
 const sensible = require('@fastify/sensible');
 const cors = require('@fastify/cors');
+const cookies = require('@fastify/cookie');
 const { PrismaClient } = require('@prisma/client');
 const app = fastify();
 
@@ -10,14 +11,84 @@ app.register(cors, {
   origin: process.env.CLIENT_URL,
   credentials: true,
 });
+app.register(cookies, { secret: process.env.COOKIE_SECRET });
+const userId = async () => (await prisma.user.findFirst({ where: { name: 'Kyle' } })).id;
+app.addHook('onRequest', (req, res, done) => {
+  userId().then((id) => {
+    if (req.cookies.userId !== id) {
+      req.cookies.userId = id;
+      res.clearCookie('userId');
+      res.setCookie('userId', id);
+    }
+    done();
+  });
+});
+
+const COMMENT_SELECT_FIELDS = {
+  id: true,
+  message: true,
+  parentId: true,
+  createdAt: true,
+  user: {
+    select: {
+      id: true,
+      name: true,
+    },
+  },
+};
 app.get('/posts', async (req, res) => {
-  return commitToDb(
+  return await commitToDb(
     prisma.post.findMany({
       select: {
         id: true,
         title: true,
       },
     })
+  );
+});
+
+app.get('/post/:id', async (req, res) => {
+  return await commitToDb(
+    prisma.post
+      .findUnique({
+        where: { id: req.params.id },
+        select: {
+          title: true,
+          body: true,
+          comments: {
+            orderBy: {
+              createdAt: 'desc',
+            },
+            select: {
+              ...COMMENT_SELECT_FIELDS,
+              _count: { select: { likes: true } },
+            },
+          },
+        },
+      })
+      .then(async (post) => {
+        try {
+          const likes = await prisma.like.findMany({
+            where: {
+              userId: req.cookies.userId,
+              commentId: { in: post.comments.map((comment) => comment.id) },
+            },
+          });
+          return {
+            ...post,
+            comments: post.comments.map((comment) => {
+              const { _count, ...restOfFields } = comment;
+              return {
+                ...restOfFields,
+                likeByMe: likes.find((like) => like.commentId === comment.id)?.userId === req.cookies.userId,
+                likeCount: _count.likes,
+              };
+            }),
+          };
+        } catch (err) {
+          return app.httpErrors.internalServerError(err.message);
+        }
+      })
   );
 });
 
@@ -30,7 +101,7 @@ const commitToDb = async (promise) => {
 const PORT = process.env.PORT || 8000;
 const startServer = async () => {
   try {
-    await app.listen(PORT, () => console.log(`server is up on http://localhost:${PORT}`));
+    await app.listen({ port: PORT }, () => console.log(`server is up on http://localhost:${PORT}`));
   } catch (err) {
     app.log.error(err);
     process.exit(1);
--- src/services/posts.js
+++ src/services/posts.js
@@ -3,3 +3,7 @@ import { makeRequest } from './makeRequest';
 export const getPosts = () => {
   return makeRequest('/posts');
 };
+
+export const getPost = (id) => {
+  return makeRequest(`/post/${id}`);
+};
